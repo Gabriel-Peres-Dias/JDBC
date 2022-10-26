9a9e5cc69a39d6879fe5684e6ba43b5c9e95df48
Mona Lisa <gabriel.2000mourad@gmail.com>
2022-10-26 20:05:39 -0300

    Ajustado o método de criar os produtos com rollback caso alguma transação não seja efetuada.

--- src/banco/TestaInsercaoComParametros.java
+++ src/banco/TestaInsercaoComParametros.java
@@ -9,11 +9,23 @@ public class TestaInsercaoComParametros {
         Connection connection = connectionFactory.recuperarConexao();
         connection.setAutoCommit(false);
 
-        PreparedStatement stm =
-                connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?, ?)",
-                        Statement.RETURN_GENERATED_KEYS);
-        adicionarVariavel("SmartTV", "45 Polegadas 144hz", stm);
-        adicionarVariavel("Garrafa", "Garrafa preta fosca ecológica", stm);
+        try {
+            PreparedStatement stm =
+                    connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            adicionarVariavel("SmartTV", "45 Polegadas 144hz", stm);
+            adicionarVariavel("Garrafa", "Garrafa preta fosca ecológica", stm);
+
+            connection.commit();
+
+            stm.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Rollback executado!!");
+            connection.rollback();
+        }
+
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
