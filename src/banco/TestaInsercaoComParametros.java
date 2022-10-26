62cc6b50b290db92a2df40ab148289404978675c
Mona Lisa <gabriel.2000mourad@gmail.com>
2022-10-26 20:15:44 -0300

    Incremento do try, agora com resources, explicitando assim no fim da necessidade do fechamento do Statement e da Connection.

--- src/banco/TestaInsercaoComParametros.java
+++ src/banco/TestaInsercaoComParametros.java
@@ -6,26 +6,20 @@ public class TestaInsercaoComParametros {
 
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection connection = connectionFactory.recuperarConexao();
-        connection.setAutoCommit(false);
+        try (Connection connection = connectionFactory.recuperarConexao()) {
+            connection.setAutoCommit(false);
 
-        try {
-            PreparedStatement stm =
-                    connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?, ?)",
-                            Statement.RETURN_GENERATED_KEYS);
-            adicionarVariavel("SmartTV", "45 Polegadas 144hz", stm);
-            adicionarVariavel("Garrafa", "Garrafa preta fosca ecológica", stm);
-
-            connection.commit();
-
-            stm.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Rollback executado!!");
-            connection.rollback();
+            try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS)) {
+                adicionarVariavel("SmartTV", "45 Polegadas 144hz", stm);
+                adicionarVariavel("Garrafa", "Garrafa preta fosca ecológica", stm);
+                connection.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Rollback executado!!");
+                connection.rollback();
+            }
         }
-
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
@@ -33,12 +27,11 @@ public class TestaInsercaoComParametros {
         stm.setString(2, descricao);
         stm.execute();
 
-        ResultSet rst = stm.getGeneratedKeys();
-        while (rst.next()) {
-            Integer id = rst.getInt(1);
-            System.out.println("ID: " + id);
+        try (ResultSet rst = stm.getGeneratedKeys()) {
+            while (rst.next()) {
+                Integer id = rst.getInt(1);
+                System.out.println("ID: " + id);
+            }
         }
-        rst.close();
     }
-
 }
