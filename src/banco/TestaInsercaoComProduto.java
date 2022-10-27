f9d0ea709135fff494fe58f59176ce9c9f574f91
Mona Lisa <gabriel.2000mourad@gmail.com>
2022-10-26 21:15:20 -0300

    Criando uma classe model do produto, e uma classe para a inserção no banco usando como referencia essa classe.

---
+++ src/banco/TestaInsercaoComProduto.java
@@ -0,0 +1,31 @@
+package banco;
+
+import banco.model.Produto;
+
+import java.sql.*;
+
+public class TestaInsercaoComProduto {
+    public static void main(String[] args) throws SQLException {
+
+        Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
+
+        try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+            String sql = "INSERT INTO PRODUTO(NOME, DESCRICAO) VALUES (?, ?)";
+
+            try (PreparedStatement preparedStatement =
+                         connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, comoda.getNome());
+                preparedStatement.setString(2, comoda.getDescricao());
+
+                preparedStatement.execute();
+
+                try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                    while (resultSet.next()) {
+                        comoda.setId(resultSet.getInt(1));
+                    }
+                }
+            }
+        }
+        System.out.println(comoda);
+    }
+}
